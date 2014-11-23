5793541817dd6ec66954720f0879426391cbf0eb
qubex22 <joroga2212@gmail.com>
2014-11-23 17:00:32 +0100

    app: CameraNext

---
+++ configs/common.mk
@@ -0,0 +1,7 @@
+
+# Path for prebuilts
+LOCAL_PATH:= vendor/qx/prebuilts/common/system
+
+# CameraNext
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/app/CameraNext/CameraNext.apk:system/app/CameraNext/CameraNext.apk
